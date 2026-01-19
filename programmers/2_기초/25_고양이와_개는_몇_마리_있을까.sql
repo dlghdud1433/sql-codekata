32b85436ca6d442b0ee67827cd89216716c128cb
dlghdud1433 <dlghdud1433@gmail.com>
2026-01-19 23:11:07 +0900

    [프로그래머스] 기초 - 고양이와 개는 몇 마리 있을까 (#25)

---
+++ programmers/2_기초/25_고양이와_개는_몇_마리_있을까.sql
@@ -0,0 +1,12 @@
+-- 고양이와 개는 몇 마리 있을까
+-- 프로그래머스 기초 (⭐⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59040
+-- 작성자: 이호영
+-- 작성일: 2026. 01. 19. 23:11:05
+
+-- 코드를 입력하세요
+SELECT animal_type,
+       count(*) as count
+from animal_ins
+group by animal_type
+order by animal_type asc;
